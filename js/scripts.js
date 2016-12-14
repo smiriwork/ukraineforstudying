434ead3d00087f80e0ec5334b280b5db7a0fdcbd
El Mehdi Smiri <hipsdog@gmail.com>
2016-12-14 15:41:51 +0100

    first commit
    migration from another repository

---
+++ js/scripts.js
@@ -0,0 +1,194 @@
+﻿jQuery(function($){
+
+'use strict';
+
+
+
+    // ----------------------------------------------
+    // Preloader
+    // ----------------------------------------------
+	(function () {
+	    $(window).load(function() {
+	        $('#pre-status').fadeOut();
+	        $('#st-preloader').delay(350).fadeOut('slow');
+	    });
+	}());
+
+
+
+    // ----------------------------------------------
+    //  magnific-popup
+    // ----------------------------------------------
+	(function () {
+
+		$('.portfolio-items').magnificPopup({
+			delegate: 'a',
+			type: 'image',
+			// other options
+			closeOnContentClick: false,
+			closeBtnInside: false,
+			mainClass: 'mfp-with-zoom mfp-img-mobile',
+
+			gallery: {
+				enabled: false
+			},
+			zoom: {
+				enabled: true,
+				duration: 300, // don't foget to change the duration also in CSS
+				opener: function(element) {
+					return element.find('i');
+				}
+			}
+
+		});
+
+	}());
+
+
+
+    // ----------------------------------------------
+    //  Isotope Filter
+    // ----------------------------------------------
+	(function () {
+		var winDow = $(window);
+		var $container=$('.portfolio-items');
+		var $filter=$('.filter');
+
+		try{
+			$container.imagesLoaded( function(){
+				$container.show();
+				$container.isotope({
+					filter:'*',
+					layoutMode:'masonry',
+					animationOptions:{
+						duration:750,
+						easing:'linear'
+					}
+				});
+			});
+		} catch(err) {
+		}
+
+		winDow.bind('resize', function(){
+			var selector = $filter.find('a.active').attr('data-filter');
+
+			try {
+				$container.isotope({
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+			}
+			return false;
+		});
+
+		$filter.find('a').click(function(){
+			var selector = $(this).attr('data-filter');
+
+			try {
+				$container.isotope({
+					filter	: selector,
+					animationOptions: {
+						duration: 750,
+						easing	: 'linear',
+						queue	: false,
+					}
+				});
+			} catch(err) {
+
+			}
+			return false;
+		});
+
+
+		var filterItemA	= $('.filter a');
+
+		filterItemA.on('click', function(){
+			var $this = $(this);
+			if ( !$this.hasClass('active')) {
+				filterItemA.removeClass('active');
+				$this.addClass('active');
+			}
+		});
+	}());
+
+
+	// -------------------------------------------------------------
+    // masonry
+    // -------------------------------------------------------------
+
+    (function () {
+		var $container = $('.portfolio-items');
+		// initialize
+		$container.masonry({
+		  itemSelector: '.work-grid'
+		});
+    }());
+
+
+  	// -------------------------------------------------------------
+    // Animated scrolling / Scroll Up
+    // -------------------------------------------------------------
+
+    (function () {
+        $('li a[href*=#]').bind("click", function(e){
+            var anchor = $(this);
+            $('html, body').stop().animate({
+                scrollTop: $(anchor.attr('href')).offset().top -79
+            }, 1000);
+            e.preventDefault();
+        });
+    }());
+
+
+    // ----------------------------------------------
+    // Owl Carousel
+    // ----------------------------------------------
+	(function () {
+
+		$(".st-testimonials").owlCarousel({
+		singleItem:true,
+		lazyLoad : true,
+		pagination:false,
+		navigation : false,
+		autoPlay: true,
+		});
+
+	}());
+
+
+    // -------------------------------------------------------------
+    // Back To Top
+    // -------------------------------------------------------------
+
+    (function () {
+        $(window).scroll(function() {
+            if ($(this).scrollTop() > 100) {
+                $('.scroll-up').fadeIn();
+            } else {
+                $('.scroll-up').fadeOut();
+            }
+        });
+    }());
+
+
+    // ----------------------------------------------
+    // Parallax Scrolling
+    // ----------------------------------------------
+	(function () {
+		$(window).bind('load', function () {
+			parallaxInit();
+		});
+		function parallaxInit() {
+			$("#testimonial").parallax("50%", 0.3);
+		}
+		parallaxInit();
+	}());
+
+
+
+});
